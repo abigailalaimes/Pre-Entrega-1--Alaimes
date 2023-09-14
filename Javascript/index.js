f5786a2cda66fe51d5ced3ea4cea90df932437ee
abigailalaimes <alaimes.a@outlook.com>
2023-09-14 20:21:21 -0300

    Prueba

--- Javascript/index.js
+++ Javascript/index.js
@@ -15,19 +15,18 @@ if (acceso === "travesura-realizada") {
 
     switch (eleccion) {
       case 1:
-        alert('Título: ${libroUno.saga}\nAutor: ${libroUno.autor}\nEstás de suerte, tenemos suficiente stock!');
+        alert(`Título: ${libroUno.saga}\nAutor: ${libroUno.autor}\nEstás de suerte, tenemos suficiente stock!`);
         break;
       case 2:
-        alert('Título: ${libroDos.saga}\nAutor: ${libroDos.autor}\n¡Apúrate! Quedan 2 unidades.');
+        alert(`Título: ${libroDos.saga}\nAutor: ${libroDos.autor}\n¡Apúrate! Quedan 2 unidades.`);
         break;
       case 3:
-        alert('Título: ${libroTres.saga}\nAutor: ${libroTres.autor}\nLo sentimos, no tenemos stock por el momento.');
+        alert(`Título: ${libroTres.saga}\nAutor: ${libroTres.autor}\nLo sentimos, no tenemos stock por el momento.`);
         break;
       default:
         alert('Error. Ingresa una opción válida.');
         break;
     }
-    acceso = prompt("Ingresa la clave de socios para ingresar al sitio");
   }
 } else {
   alert('Contraseña incorrecta. Verifica los datos ingresados.');
