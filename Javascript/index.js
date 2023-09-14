2748d33852b9d2b20c845d8799f39ebe79c2d29f
abigailalaimes <alaimes.a@outlook.com>
2023-09-14 19:14:02 -0300

    Cambio de trabajo

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,35 +1,37 @@
-const access = prompt("Ingresa la clave de socios para ingresar al sitio")
+const acceso = prompt("Ingresa la clave de socios para ingresar al sitio");
 
-if (access === "travesura-realizada") {
-    const libroUno =new 
+if (acceso === "travesura-realizada") {
+  function RevelarStock(saga, autor) {
+    this.saga = saga;
+    this.autor = autor;
+  }
 
-}
-function mostrarStock (a, operation) {
-    While (access === "travesura-realizada"); {
-        let libros = parseInt(prompt("Ingrese el número de la saga que desea adquerir para verificar stock (ACOTAR(1)- HARRY POTTER(2)- LOS JUEGOS DEL HAMBRE(3)"));
+  const libroUno = new RevelarStock("Acotar", "Sara J Maas");
+  const libroDos = new RevelarStock("Harry Potter", "J. K. Rowling");
+  const libroTres = new RevelarStock("Los juegos del hambre", "Suzanne Collins");
 
-            switch (libros){
-            case "1":
-               alert("Estás de suerte, tenemos sufiente stock!!")
-            break;
-            case "2":
-                alert("Apurate! Quedan 2 unidades!")
-            break;
-            case "3":
-                alert("Lo sentimos! No tenemos stock por el momento.");
-            break;
-            default:
-                alert('Error! Ingrese una opción válida');
-            break;            
-        }
-            access = prompt("Ingresa la clave de socios para ingresar al sitio");
-            else {
-        alert('Contraseña incorrecta. Verifica los datos ingresados.')
+  while (acceso === "travesura-realizada") {
+    let eleccion = parseInt(prompt("Ingrese el número de la saga que desea adquirir para verificar stock (ACOTAR(1)- HARRY POTTER(2)- LOS JUEGOS DEL HAMBRE(3))"));
+
+    switch (eleccion) {
+      case 1:
+        alert(`Título: ${libroUno.saga}\nAutor: ${libroUno.autor}\nEstás de suerte, tenemos suficiente stock!`);
+        break;
+      case 2:
+        alert(`Título: ${libroDos.saga}\nAutor: ${libroDos.autor}\n¡Apúrate! Quedan 2 unidades.`);
+        break;
+      case 3:
+        alert(`Título: ${libroTres.saga}\nAutor: ${libroTres.autor}\nLo sentimos, no tenemos stock por el momento.`);
+        break;
+      default:
+        alert('Error. Ingresa una opción válida.');
+        break;
     }
+    acceso = prompt("Ingresa la clave de socios para ingresar al sitio");
+  }
+} else {
+  alert('Contraseña incorrecta. Verifica los datos ingresados.');
 }
-
-mostrarStock (numA, operation);
-
 
 
 
